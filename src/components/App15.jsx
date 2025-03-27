0f8fd4ca2b7a81ab740bca854d8ca50f89738681
srujana345 <sai.srujana144@gmail.com>
2025-03-27 17:36:43 +0530

    add

--- src/components/App15.jsx
+++ src/components/App15.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { FaHome,FaAddressBook  } from "react-icons/fa";
+import { FaHome,FaAddressBook,FaCarrot  } from "react-icons/fa";
+import { AiFillAlert } from "react-icons/ai";
 export default function App15() {
   return (
     <div>
       <h3>This is App15</h3>
       <div>
-        <FaHome size={15} />
+        <FaHome size={20} />
         <label>Home</label>
       </div>
       <div>
-        <FaAddressBook size={15}/>
+        <FaAddressBook size={20}/>
         <label>Contact</label>
+      </div>
+      <div>
+        <FaCarrot size={20}/>
+        <label>Carrot</label>
+      </div>
+      <div>
+        <AiFillAlert size={20} color="red"/>
+        <label>Alert</label>
       </div>
     </div>
   );
